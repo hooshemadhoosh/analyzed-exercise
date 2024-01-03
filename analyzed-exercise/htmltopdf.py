43e3d5df2d03d3d772672bc93e1444b99b33c1c1
Ehsan <89688597+3eh3an@users.noreply.github.com>
2024-01-03 18:52:47 +0330

    Add files via upload

---
+++ analyzed-exercise/htmltopdf.py
@@ -0,0 +1,141 @@
+import pickle
+from bs4 import BeautifulSoup
+import re
+import random
+
+
+def load_object(filename):
+    try:
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except Exception as ex:
+        print("Error during unpickling object (Possibly unsupported):", ex)
+
+def make_img_tag(address : str):
+    str_img_tag = '<img src="./src/images/Exercise.jpg" class="h-[9vw]" alt="exercise-pic">'
+    pattern = re.compile(r'src=".+" class=') #the pattern which i want to search for
+    result = re.search(pattern , str_img_tag) 
+    range1 = result.span() #returns a range of matched substring
+    str_img_tag = str_img_tag.replace(str_img_tag[range1[0] + 4 : range1[1]-7] , f'{address}') #replacing last address with a new one!
+    return str_img_tag
+
+data = load_object('data')
+
+
+#import pprint 
+#pprint.PrettyPrinter(indent=4,width=30).pprint(data)    
+#data = {'40230853' : {'BMI' : 'Thin' , 'Program' : [[[('excercise nameA1' , ['first adress' , 'second address']) , ('excersise nameA2' , []) , (...)] , [('excerciseB1' , ['address1' , 'address2'])]] , [] , []] }}
+
+
+
+#ATTENTION! THE student number isn't string! it is an unknown type! 
+for student_number in data:
+    htmlfile = open('sport-program-build\index.html' , 'r' , encoding='utf8')
+    index = htmlfile.read()
+    soup = BeautifulSoup(index , 'html.parser')
+
+    #------general information------------
+    age = 402 - int(str(student_number)[:3]) + 18
+    bmi = str(data[student_number]['BMI'])
+
+    #finding the paragraph tag which contains the name of the person
+    nameee = soup.find('p' , {'id' : 'name-content'})
+
+    #changing the p tag where it contains some letters and replace it with the string form of the student_number!
+    new_text = nameee.find(text=re.compile(r'^[\u0600-\u06FF\s]+$')).replace_with(str(student_number))
+    #'^[\u0600-\u06FF\s]+$' THIS IS A REGEX FOR ALL PERSIAN/ARRABIC LETTERS!
+
+    agechanging = soup.find('p' , {'id':'age-content'}).find(text=re.compile(r'\d+')).replace_with(str(age))
+
+    bmichange =  soup.find('p' , {'id':'BMI-content'}).find(text=re.compile(r'\d+')).replace_with(bmi)
+
+
+    #JUNK CODES! YOU MAY NEED THEM!
+    #age = nameee[1] 
+    #name = nameee[3]
+    #name = BeautifulSoup(str(name) , 'html.parser').p
+    #name.string = str(student_number)
+    student = data[student_number]
+    for i in range(5):
+        ind_pr = random.randint(0,2)
+        exc_program = student['Program'][ind_pr]
+        #merging all type of exercises in one list which contains some tuples 
+        #whose first element is the name of exercise and the second one is a tuple of image addresses
+        exercises = [e for k in exc_program for e in k] 
+
+        if i == 0:
+            day_num = 'روز اول'
+        elif i == 1:
+            day_num = 'روز دوم'
+        elif i ==2:
+            day_num = 'روز سوم'
+        elif i == 3:
+            day_num = 'روز چهارم'
+        else:
+            day_num = 'روز پنجم' 
+        #making table tage
+        table = BeautifulSoup('<div class="divide-y divide-gray-300"></div>' , 'html.parser')
+        table_header_string =   '''
+                <!-- Table Header -->
+                <div class="space-y-2 relative">
+                    <div class="flex items-center justify-center divide-x-2 divide-x-reverse divide-gray-300 text-sm font-bold">
+                        <span class="pl-6 py-1 w-[75px]">روز اول</span>
+                        <span class="px-6 py-1 w-[72px]">ردیف</span>
+                        <span class="px-6 py-1 w-[134px] text-center">نام حرکت</span>
+                        <span class="pr-6 py-1 w-[75px]">تعداد</span>
+                    </div>
+                    <div class="seperating-line"></div>
+                </div>
+                                '''
+        table_header = BeautifulSoup(table_header_string , 'html.parser')
+        commit_day_number = table_header.find('span' , {'class' : 'pl-6 py-1 w-[75px]'}).find(text=re.compile(r'.+')).replace_with(day_num)
+        rownum = 0
+        for exercise in exercises:
+            rownum += 1
+            #making the tag of a row of table
+            row_html_str ="""<div class="space-y-2 pt-4 tbl-row">
+                                <div class="space-y-2">
+                                    <div class="flex items-center justify-center divide-x-2 divide-x-reverse divide-gray-300 text-xs">
+                                        <span class="pl-6 py-1 w-[72px]" id="row-number">1</span>
+                                        <span class="px-6 py-1 w-[134px] text-center" id="exercise-name">پلانک</span>
+                                        <span class="pr-6 py-1 w-[75px]" id="sets-numbers-perset">3×12</span>
+                                    </div>
+
+                                    <div class="flex-center gap-x-4">
+                                    </div>
+                                </div>
+                            </div>"""
+            row = BeautifulSoup(row_html_str , 'html.parser')
+
+            #change the exercise name with a new one
+            tempor = row.find('span' , {'id':'exercise-name'}).find(text=re.compile(r'.+')).replace_with(str(exercise[0]))
+
+            all_str_img_tags = ''
+            if len(exercise[1]) > 0:
+                for i in exercise[1]:
+                    str_img_tag = make_img_tag('../Whole_body/Beginner/' + str(i))
+                    all_str_img_tags += str_img_tag
+            else:
+                all_str_img_tags = make_img_tag('./src/images/Exercise.jpg')
+
+            #we have all image tags here!
+            img_tag = BeautifulSoup(all_str_img_tags , 'html.parser')
+
+            commit_img_tag = row.find('div' , {'class' : 'flex-center gap-x-4'}).append(img_tag)
+            commit_row_num = row.find('span' , {'id':'row-number'}).find(text=re.compile(r'\d+')).replace_with(str(rownum))
+            commit_exer_name = row.find('span' , {'id':"exercise-name"}).find(text=re.compile(r'^[\u0600-\u06FF\s]+$')).replace_with(str(exercise[0]))
+
+            table.div.append(row)
+
+        soup.body.section.div.find('div', {'id':'table-container'}).append(table_header)
+        soup.body.section.div.find('div', {'id':'table-container'}).append(table)
+        #print(f'---------------THIS IS THE {i}th table-----------------\n' ,table.prettify())
+                                #i use \\ instead of \ beacause if it doesn't it made error
+    with open("sport-program-build\\" + f'newFile{student_number}.html' , 'wb') as f:
+        f.write(soup.prettify("utf-8"))
+
+
+
+
+#html_string = html_string.find_all('img' , src=re.compile(r".+"))
+#print (type(html_string[0]))
---
+++ analyzed-exercise/reader.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import pickle
+from os import listdir
+import random
+kind = {'A':"Whole_body"}
+level = ['Beginner','Intermediate','Advanced']
+# program = {"Thin":['1A1B1C1D3E1F','2A2B1C1D4E2F','3A3B1C1D5E3F'],
+#            "Fat":['3A1B1C1D1E1F','5A1B1C1D2E2F','7A2B1C1D2E3F'],
+#            "Normal":['2A1B1C1D2E1F','3A2B1C1D3E2F','4A2B2C1D4E3F']}
+program = {"Thin":['1A','2A','3A'],
+           "Fat":['3A2A','5A1A','7A3A'],
+           "Normal":['2A','3A','4A']}
+def save_object(obj,filename):
+    try:
+        with open(filename, "wb") as f:
+            pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+    except Exception as ex:
+        print("Error during pickling object (Possibly unsupported):", ex)
+def load_object(filename):
+    try:
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except Exception as ex:
+        print("Error during unpickling object (Possibly unsupported):", ex)
+def get_exercise_names(dir0):
+    print(dir0)
+    file = pd.read_csv(dir0+'dictionary.csv',encoding='ISO-8859-6')
+    dictionary = {str(file['code'][i]):file['name'][i] for i in range(len(file['code']))}
+    names = [name for name in listdir(dir0) if name.endswith('.jpg')]
+    result = {key:[] for key in dictionary}
+    for name in names:
+        code = name.split('_')[0]
+        try:
+            result[code]+= [name]
+        except:
+            print("Name is not defined for a file...")
+    result = {dictionary[key]:tuple(value) for key,value in result.items()}
+    return result
+dirs = {}
+for key,value in kind.items():
+    for lev in level:
+        dirs[key+lev]=list(get_exercise_names(value+'\\'+lev+'\\').items())
+def get_programs(bmi):
+    result = []
+    for i,pro in enumerate(program[bmi]):
+        res = []
+        for j in range(len(pro)//2):
+            name_picture_pair = dirs[pro[2*j+1]+level[i]]
+            
+            res += [random.choices(name_picture_pair, k=int(pro[2*j]) )]
+        result.append(res)
+    return result
+
+
+
+file = pd.read_csv('info.csv')
+print(file)
+data = {file['ID'][i]:{'BMI':file['BMI'][i]} for i in range(len(file['ID'])) }
+for key,value in data.items():
+    temp = value
+    temp['Program']=get_programs(value['BMI'])
+    data[key]=temp
+
+
+
+# import pprint
+# pprint.PrettyPrinter(indent=4,width=30).pprint(dirs) 
+# pprint.PrettyPrinter(indent=4,width=30).pprint(data)    
+save_object(data,'data')
