6f69987b9efb078312b0efbeeacae21e07f9eb34
Abolfazl <dahaghaynhmh@gmail.com>
2024-01-09 02:25:39 +0330

    ...

--- pdf.py
+++ pdf.py
@@ -2,4 +2,4 @@ from pyhtml2pdf import converter
 from sympy import true
 source = r'D:\Projects\analyzed-exercise\sport-program-build-new-version\newFile0.html'
 target = 'index.pdf'
-converter.convert(source,target,timeout=2,compress=true,power=3,install_driver=False)
+converter.convert(source,target,timeout=1,compress=true,power=0)
