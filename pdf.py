5dfa11398059597ef9d5f6fd67a1d6f35801862a
Abolfazl <dahaghaynhmh@gmail.com>
2024-01-12 21:01:59 +0330

    code cleaned up

--- pdf.py
+++ pdf.py
@@ -4,7 +4,7 @@ from subprocess import Popen,PIPE
 from os import listdir
 import tkinter as tk
 from tkinter import filedialog
-
+import os
 def html_to_pdf(html_file_name,pdf_file_name):
     command = f"chrome --headless --disable-gpu --print-to-pdf={pdf_file_name} --no-pdf-header-footer --run-all-compositor-stages-before-draw --no-margins --no-sandbox {html_file_name}"
     process = Popen(command.split(), stdout=PIPE, stderr=PIPE,shell=True)
@@ -20,6 +20,9 @@ def html_to_pdf2(html_file_name,pdf_file_name):
 root = tk.Tk()
 root.withdraw()
 directory = filedialog.askdirectory()+'/'
-names = [name for name in listdir(directory) if name.endswith('.html')]
-for name in names:
-    html_to_pdf(directory+name,directory+'PDF_Files/'+name.replace('.html','.pdf'))
+result_folder = 'PDF_Files/'
+if directory!="":
+    if not os.path.exists(directory+result_folder):  os.mkdir(directory+result_folder)
+    names = [name for name in listdir(directory) if name.endswith('.html')]
+    for name in names:
+        html_to_pdf(directory+name,directory+result_folder+name.replace('.html','.pdf'))
