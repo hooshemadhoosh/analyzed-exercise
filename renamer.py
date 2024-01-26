b2a2ea2214abdc8ad83a713b546044e0242ca873
Abolfazl <dahaghaynhmh@gmail.com>
2024-01-26 20:00:56 +0330

    Renamer Updated, Duplicate remover added and fitness folder updated

--- renamer.py
+++ renamer.py
@@ -1,4 +1,4 @@
-from os import listdir,rename
+from os import listdir,rename,walk
 import tkinter as tk
 from tkinter import filedialog
 
@@ -6,22 +6,27 @@ root = tk.Tk()
 root.withdraw()
 
 directory = filedialog.askdirectory()
-print (directory)
 
-result = '\ufeff'
-result += 'code,name\n'
 
-paths = [name for name in listdir(directory) if name.endswith('.jpg')]
-data = {key.split('_')[0]:[] for key in paths}
-for path in paths:
-    data[path.split('_')[0]]+=[directory+'/'+path]
+def renamer(directory):
+    print (directory)
+    result = '\ufeff'
+    result += 'code,name\n'
 
-for i,dic in enumerate(data.items()):
-    result+= f"{i+1},{dic[0]}\n"
-    for path in dic[1]:
-        newpath = path.replace(dic[0],str(i+1))
-        # print(path,newpath)
-        rename(path,newpath)
-with open(directory+'/dictionary.csv','w',encoding='UTF-8') as f:
-    f.write(result)
-print(result)
+    paths = [name for name in listdir(directory) if name.endswith('.jpg')]
+    data = {key.split('_')[0]:[] for key in paths}
+    for path in paths:
+        data[path.split('_')[0]]+=[directory+'/'+path]
+
+    for i,dic in enumerate(data.items()):
+        result+= f"{i+1},{dic[0]}\n"
+        for path in dic[1]:
+            newpath = path.replace(dic[0],str(i+1))
+            # print(path,newpath)
+            rename(path,newpath)
+    with open(directory+'/dictionary.csv','w',encoding='UTF-8') as f:
+        f.write(result)
+    print(result)
+
+for dir_path, dir_names, file_names in walk(directory):
+    if len(file_names): renamer(dir_path)
